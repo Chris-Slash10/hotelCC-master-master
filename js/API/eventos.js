7eacbe1680b47d05f23bd83c2dbe14a471b6295c
Chris-Slash10 <chris.slash.sambora@gmail.com>
2013-08-10 09:51:29 -0500

    nuevo

---
+++ js/API/eventos.js
@@ -0,0 +1,21 @@
+//Eventos
+$(document).ready(function(){
+	document.addEventListener("deviceready", function(){
+		window.location.href = "#login";
+		$('#regSend').tap(function(){
+			var nom = $('#regNom').val();
+			var mail = $('#regEmail').val();
+			var tel = $('#regTel').val();
+            var foto = $('#regFoto').attr('rel');
+			
+			if(nom != '' && mail != '' && tel != '' && foto != undefined && foto != ''){
+				subirDatos(nom,mail,tel,foto);
+			}else{
+				navigator.notification.alert('Todos los campos son requeridos', null, 'Registro','Aceptar');
+			}
+		});
+        $('#regFoto').tap(function(){
+            tomarFoto();
+        });
+	}, false);
+});
---
+++ js/API/servidor.js
@@ -0,0 +1,22 @@
+function subirDatos(nom,email,tel,foto){
+    $.ajax({
+        type: "POST",
+        url: "http://igitsoft.com/pgtest.php",
+        data: 'nom='+nom+'&mai='+email+'&tel='+tel
+    }).done(function(msg) {
+        if(msg==1){
+            navigator.notification.confirm('Datos subidos Correctamente', function(btn){
+                switch(btn){
+                    case 1:
+                        navigator.notification.vibrate(500);
+                        break;
+                    case 2:
+                        navigator.notification.beep(2);
+                        break;
+                }
+            },'Confirmación','Vibrar, Beep, Cancelar');
+        }else{
+            navigator.notification.alert('Error al Procesar Datos', null, 'Error','Aceptar');
+        }
+    });
+}
